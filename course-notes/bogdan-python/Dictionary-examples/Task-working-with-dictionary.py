7bdda99e0d75c04390e8083d7f2446773a7b99c9
Adil Hafeez <adilhafeez299@gmail.com>
2025-11-04 14:40:35 +0000

    env setup and initial python fundamentals test script

---
+++ course-notes/bogdan-python/Dictionary-examples/Task-working-with-dictionary.py
@@ -0,0 +1,21 @@
+user_dict = {}
+
+key1 = input("Enter the name of the first key:")
+value1 = input("Enter the value for this key:")
+user_dict[key1] = value1
+
+key2 = input("Enter the name of the second key:")
+value2 = input("Enter the value for this key:")
+user_dict[key2] = value2
+
+key3 = input("Enter the name of the third key:")
+value3 = input("Enter the value for this key:")
+user_dict[key3] = value3
+
+#show results
+print("\nFinal dictionary:")
+print(user_dict)
+
+# go over this again, this relied on input, need 3 name, 3 values, so need to repeat sequence 3 times
+# can use loop - wait to learn about loops and how they can be applied to this exercise
+
