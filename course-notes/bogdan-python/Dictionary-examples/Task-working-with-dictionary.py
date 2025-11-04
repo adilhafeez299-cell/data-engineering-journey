b25ec2055689158f66cf75991162683dd5847c08
Adil Hafeez <adilhafeez299@gmail.com>
2025-11-04 14:43:39 +0000

    env setup and initial python fundamentals test script

--- course-notes/bogdan-python/Dictionary-examples/Task-working-with-dictionary.py
+++ course-notes/bogdan-python/Dictionary-examples/Task-working-with-dictionary.py
@@ -19,3 +19,19 @@ print(user_dict)
 # go over this again, this relied on input, need 3 name, 3 values, so need to repeat sequence 3 times
 # can use loop - wait to learn about loops and how they can be applied to this exercise
 
+#Alternative solution:
+user_dict = {}
+
+key1 = input("Key 1: ")
+value1 = input("Value 1: ")
+user_dict[key1] = value1
+
+key2 = input("Key 2: ")
+value2 = input("Value 2: ")
+user_dict[key2] = value2
+
+key3 = input("Key 3: ")
+value3 = input("Value 3: ")
+user_dict[key3] = value3
+
+print(user_dict)
