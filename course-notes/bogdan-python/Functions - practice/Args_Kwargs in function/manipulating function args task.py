f97cbb6595bafb79edf849c52188463557be5eb8
Adil Hafeez <adilhafeez299@gmail.com>
2025-11-08 18:33:26 +0000

    env setup and initial python fundamentals test script

--- course-notes/bogdan-python/Functions - practice/Args_Kwargs in function/manipulating function args task.py
+++ course-notes/bogdan-python/Functions - practice/Args_Kwargs in function/manipulating function args task.py
@@ -31,3 +31,15 @@ result4 = merge_lists_to_dict_kwargs(list_1=[1, 2, 3], list2=['a', 'b', 'c'])
 print(result4)
 
 # need to go over again as I was definitely a bit confused!!
+
+#Task 2
+# create an update_car_info function in which all named arguments will be combined into a car dictionary
+# Add a new is_available key to the dictionary with value True
+# return modfied dictionary from the function
+def update_car_info(**kwargs):
+    car_info = kwargs
+    car_info['is_available'] = True
+    return car_info
+
+car = update_car_info(brand = 'Toyota', model = 'Camry', year = 2020)
+print(car)
