cdaf4c77955b11715da7afa8aa306740d28940ad
Adil Hafeez <adilhafeez299@gmail.com>
2026-01-30 11:44:23 +0000

    env setup and initial python fundamentals test script

---
+++ 01-phase-python/bogdan-course/what_is__name__and__main__/packages_practice.py
@@ -0,0 +1,12 @@
+#from my_package import module_1, module_2
+# instead of this from my_package import module_2
+
+from my_package.module_1 import my_num
+from my_package.module_2 import my_name
+
+print(my_num)
+print(my_name)
+
+
+#print(module_1.my_num)
+#print(module_2.my_name)
