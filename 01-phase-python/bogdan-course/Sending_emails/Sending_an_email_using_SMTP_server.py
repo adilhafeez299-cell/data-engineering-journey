1d7afc439a38ddd21035ab4d64915fe358801dfd
Adil Hafeez <adilhafeez299@gmail.com>
2026-02-08 16:14:40 +0000

    env setup and initial python fundamentals test script

--- 01-phase-python/bogdan-course/Sending_emails/Sending_an_email_using_SMTP_server.py
+++ 01-phase-python/bogdan-course/Sending_emails/Sending_an_email_using_SMTP_server.py
@@ -5,14 +5,17 @@ from pathlib import Path
 
 
 html_template = Template(Path('Templates/Template_html_file.html').read_text())
-print(html_template)
-print(html_template.is_valid())
+# print(html_template)
+# print(html_template.is_valid())
+
+
+html_content = html_template.substitute({'name': 'Adil', 'date': 'tomorrow'})
 
 my_email = EmailMessage()
 my_email['From'] = 'Adil <Adil@example.com>'
 my_email['To'] = 'test@test.com'
 my_email['Subject'] = 'Hello from Python'
-my_email.set_content('Hey! How are you doing?!')
+my_email.set_content(html_content, 'html')
 
 with smtplib.SMTP(host='localhost', port=2525) as smtp_server:
     smtp_server.ehlo()
